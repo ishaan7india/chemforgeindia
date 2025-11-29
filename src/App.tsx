9d87bcc21bc480e846e5c18b228b0433e001e89a
CDT Ishaan <percyjackson7.riptide@gmail.com>
2025-11-29 18:35:22 +0530

    Fix indentation in BrowserRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <<BrowserRouter basename="/chemforgeindia">>
+        <<BrowserRouter basename="/chemforgeindia">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
